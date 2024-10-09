116f41945b7015d330ca3e8efca25d09be473427
dmytrotsymbal <dima.tsymbal@outlook.com>
2024-10-09 13:16:22 +0300

    add formik validation

--- src/pages/Crimes/EditCrimePage.tsx
+++ src/pages/Crimes/EditCrimePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { RootState } from "../../redux/store";
 import { CriminalRecords } from "../../types/criminalRecordsTypes";
@@ -19,11 +19,25 @@ import {
   Select,
   TextField,
   Typography,
-  SelectChangeEvent,
 } from "@mui/material";
 import CustomLoader from "../../components/ui/CustomLoader";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import CustomIconButton from "../../components/ui/CustomIconButton";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+// Валидация формы с использованием Yup
+const validationSchema = Yup.object().shape({
+  article: Yup.string()
+    .required("Article is required")
+    .max(100, "Article cannot be longer than 100 characters"),
+  convictionDate: Yup.date().required("Conviction date is required"),
+  releaseDate: Yup.date().nullable().notRequired(),
+  sentence: Yup.string().required("Sentence is required"),
+  caseDetailsURL: Yup.string().url("Invalid URL format").nullable(),
+  details: Yup.string().required("Details are required"),
+  prisonID: Yup.number().required("Prison is required"),
+});
 
 const EditCrimePage = () => {
   const { crimeId } = useParams<{ crimeId: string }>();
@@ -46,7 +60,7 @@ const EditCrimePage = () => {
 
   const memoizedCrime = useMemo(() => criminalRecord, [criminalRecord]);
 
-  const [formValues, setFormValues] = useState({
+  const initialValues = {
     criminalRecordID: memoizedCrime?.criminalRecordID || 0,
     userID: memoizedCrime?.userID || "",
     article: memoizedCrime?.article || "",
@@ -55,12 +69,12 @@ const EditCrimePage = () => {
       : "",
     releaseDate: memoizedCrime?.releaseDate
       ? new Date(memoizedCrime.releaseDate).toISOString().substring(0, 10)
-      : null,
+      : "",
     sentence: memoizedCrime?.sentence || "",
     caseDetailsURL: memoizedCrime?.caseDetailsURL || "",
     details: memoizedCrime?.details || "",
     prisonID: memoizedCrime?.prison.prisonID || 0,
-  });
+  };
 
   useEffect(() => {
     if (crimeId) {
@@ -68,52 +82,11 @@ const EditCrimePage = () => {
     }
   }, [dispatch, crimeId]);
 
-  useEffect(() => {
-    if (memoizedCrime) {
-      setFormValues({
-        criminalRecordID: memoizedCrime.criminalRecordID,
-        userID: memoizedCrime.userID,
-        article: memoizedCrime.article,
-        convictionDate: memoizedCrime?.convictionDate
-          ? new Date(memoizedCrime.convictionDate)
-              .toISOString()
-              .substring(0, 10)
-          : "",
-        releaseDate: memoizedCrime?.releaseDate
-          ? new Date(memoizedCrime.releaseDate).toISOString().substring(0, 10)
-          : "",
-        sentence: memoizedCrime.sentence,
-        caseDetailsURL: memoizedCrime.caseDetailsURL || "",
-        details: memoizedCrime.details,
-        prisonID: memoizedCrime.prison.prisonID,
-      });
-    }
-  }, [memoizedCrime]);
-
-  // Обработчик для обычных полей ввода
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormValues((prevValues) => ({
-      ...prevValues,
-      [name]: value,
-    }));
-  };
-
-  // Обработчик для изменения значения в Select (тюрем)
-  const handleSelectChange = (e: SelectChangeEvent<number>) => {
-    const { name, value } = e.target;
-    setFormValues((prevValues) => ({
-      ...prevValues,
-      [name]: Number(value), // Преобразуем значение в число
-    }));
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleSubmit = async (values: typeof initialValues) => {
     if (crimeId && memoizedCrime) {
       const updatedCrime = {
         ...memoizedCrime,
-        ...formValues,
+        ...values,
       };
       await dispatch(
         updateCriminalRecord({
@@ -183,112 +156,136 @@ const EditCrimePage = () => {
         </Typography>
 
         <br />
-        <form onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Criminal ID"
-                name="criminalRecordID"
-                value={formValues.criminalRecordID}
-                disabled
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Article"
-                name="article"
-                value={formValues.article}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                fullWidth
-                label="Conviction Date"
-                name="convictionDate"
-                type="date"
-                value={formValues.convictionDate}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                fullWidth
-                label="Release Date"
-                name="releaseDate"
-                type="date"
-                value={formValues.releaseDate}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Sentence"
-                name="sentence"
-                value={formValues.sentence}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                fullWidth
-                label="Case Details URL"
-                name="caseDetailsURL"
-                value={formValues.caseDetailsURL}
-                onChange={handleInputChange}
-              />
-            </Grid>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+          enableReinitialize
+        >
+          {({ values, handleChange }) => (
+            <Form>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    label="Criminal ID"
+                    name="criminalRecordID"
+                    value={values.criminalRecordID}
+                    disabled
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    label="Article"
+                    name="article"
+                    helperText={<ErrorMessage name="article" />}
+                    value={values.article}
+                    onChange={handleChange}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    label="Conviction Date"
+                    name="convictionDate"
+                    type="date"
+                    value={values.convictionDate}
+                    onChange={handleChange}
+                    helperText={<ErrorMessage name="convictionDate" />}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    label="Release Date"
+                    name="releaseDate"
+                    type="date"
+                    value={values.releaseDate}
+                    onChange={handleChange}
+                    helperText={<ErrorMessage name="releaseDate" />}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    label="Sentence"
+                    name="sentence"
+                    value={values.sentence}
+                    onChange={handleChange}
+                    helperText={<ErrorMessage name="sentence" />}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    label="Case Details URL"
+                    name="caseDetailsURL"
+                    value={values.caseDetailsURL}
+                    onChange={handleChange}
+                    helperText={<ErrorMessage name="caseDetailsURL" />}
+                  />
+                </Grid>
 
-            <Grid item xs={6}>
-              <FormControl fullWidth>
-                <InputLabel id="prison-label">Prison</InputLabel>
-                <Select
-                  labelId="prison-label"
-                  name="prisonID"
-                  value={formValues.prisonID}
-                  onChange={handleSelectChange} // Используем новый обработчик для Select
-                >
-                  {prisonsList.map((prison) => (
-                    <MenuItem key={prison.prisonID} value={prison.prisonID}>
-                      {prison.prisonName}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Details"
-                name="details"
-                value={formValues.details}
-                onChange={handleInputChange}
-              />
-            </Grid>
-          </Grid>
+                <Grid item xs={6}>
+                  <FormControl fullWidth>
+                    <InputLabel id="prison-label">Prison</InputLabel>
+                    <Field
+                      as={Select}
+                      labelId="prison-label"
+                      name="prisonID"
+                      value={values.prisonID}
+                      onChange={handleChange}
+                    >
+                      {prisonsList.map((prison) => (
+                        <MenuItem key={prison.prisonID} value={prison.prisonID}>
+                          {prison.prisonName}
+                        </MenuItem>
+                      ))}
+                    </Field>
+                  </FormControl>
+                  <ErrorMessage name="prisonID" />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    label="Details"
+                    name="details"
+                    value={values.details}
+                    onChange={handleChange}
+                    helperText={<ErrorMessage name="details" />}
+                  />
+                </Grid>
+              </Grid>
 
-          <br />
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <Button variant="contained" color="success" type="submit">
-              Зберегти
-            </Button>
-            <Button type="reset" variant="contained" color="inherit">
-              Скинути
-            </Button>
-            <CustomIconButton
-              icon={<ArrowBackIcon />}
-              onClick={() => navigate(-1)}
-            />
-          </Box>
-        </form>
+              <br />
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Button variant="contained" color="success" type="submit">
+                  Зберегти
+                </Button>
+                <Button type="reset" variant="contained" color="inherit">
+                  Скинути
+                </Button>
+                <CustomIconButton
+                  icon={<ArrowBackIcon />}
+                  onClick={() => navigate(-1)}
+                />
+              </Box>
+            </Form>
+          )}
+        </Formik>
       </Paper>
     </>
   );
