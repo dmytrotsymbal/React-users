4667bc9cbd87d07f7a14545dd3fb181ae5e86c26
dmytrotsymbal <dima.tsymbal@outlook.com>
2024-12-12 16:53:42 +0200

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import Header from "./layouts/Header";
 import "./styles/App.css";
 import "./styles/font.css";
@@ -9,6 +9,7 @@ import ScrollToTop from "./utils/scrollToTop";
 import PrivateRoute from "./utils/PrivateRoute";
 import { useAppSelector } from "./redux/hooks";
 import { RootState } from "./redux/store";
+import Footer from "./layouts/Footer";
 
 const AuthPage = lazy(() => import("./pages/Auth/AuthPage"));
 const UsersPage = lazy(() => import("./pages/Users/UsersPage"));
@@ -28,159 +29,169 @@ const AddCrimePage = lazy(() => import("./pages/Crimes/AddCrimePage"));
 const NoAccessPage = lazy(() => import("./pages/NoAccess/NoAccessPage"));
 const ErrorPage = lazy(() => import("./pages/ErrorPage"));
 
-function App() {
+const App = () => {
   const { isLoggedIn } = useAppSelector((state: RootState) => state.auth);
   const lightTheme = useAppSelector((state) => state.theme.lightTheme);
   return (
     <>
-      <Header />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+        }}
+      >
+        <Header />
 
-      <main className={lightTheme ? "main" : "main-dark"}>
-        <ScrollToTop />
-        <Container maxWidth="xl">
-          <Suspense
-            fallback={
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100vh",
-                }}
-              >
-                <CustomLoader />
-              </div>
-            }
-          >
-            <Routes>
-              <Route path="/auth" element={<AuthPage />} />
+        <main className={lightTheme ? "main" : "main-dark"}>
+          <ScrollToTop />
+          <Container maxWidth="xl">
+            <Suspense
+              fallback={
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100vh",
+                  }}
+                >
+                  <CustomLoader />
+                </div>
+              }
+            >
+              <Routes>
+                <Route path="/auth" element={<AuthPage />} />
 
-              <Route
-                path="/users"
-                element={
-                  <PrivateRoute
-                    allowedRoles={["admin", "moderator", "visitor"]}
-                  >
-                    <UsersPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/user/:userId"
-                element={
-                  <PrivateRoute
-                    allowedRoles={["admin", "moderator", "visitor"]}
-                  >
-                    <UserDetailsPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/user/edit/:userId"
-                element={
-                  <PrivateRoute allowedRoles={["admin", "moderator"]}>
-                    <EditUserPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/user/add"
-                element={
-                  <PrivateRoute allowedRoles={["admin", "moderator"]}>
-                    <AddUserPage />
-                  </PrivateRoute>
-                }
-              />
+                <Route
+                  path="/users"
+                  element={
+                    <PrivateRoute
+                      allowedRoles={["admin", "moderator", "visitor"]}
+                    >
+                      <UsersPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/user/:userId"
+                  element={
+                    <PrivateRoute
+                      allowedRoles={["admin", "moderator", "visitor"]}
+                    >
+                      <UserDetailsPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/user/edit/:userId"
+                  element={
+                    <PrivateRoute allowedRoles={["admin", "moderator"]}>
+                      <EditUserPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/user/add"
+                  element={
+                    <PrivateRoute allowedRoles={["admin", "moderator"]}>
+                      <AddUserPage />
+                    </PrivateRoute>
+                  }
+                />
 
-              <Route
-                path="/cars"
-                element={
-                  <PrivateRoute
-                    allowedRoles={["admin", "moderator", "visitor"]}
-                  >
-                    <CarsPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/car/edit/:carId"
-                element={
-                  <PrivateRoute allowedRoles={["admin", "moderator"]}>
-                    <EditCarPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/car/add/:userId"
-                element={
-                  <PrivateRoute allowedRoles={["admin", "moderator"]}>
-                    <AddCarPage />
-                  </PrivateRoute>
-                }
-              />
+                <Route
+                  path="/cars"
+                  element={
+                    <PrivateRoute
+                      allowedRoles={["admin", "moderator", "visitor"]}
+                    >
+                      <CarsPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/car/edit/:carId"
+                  element={
+                    <PrivateRoute allowedRoles={["admin", "moderator"]}>
+                      <EditCarPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/car/add/:userId"
+                  element={
+                    <PrivateRoute allowedRoles={["admin", "moderator"]}>
+                      <AddCarPage />
+                    </PrivateRoute>
+                  }
+                />
 
-              <Route
-                path="/address/:addressId"
-                element={
-                  <PrivateRoute
-                    allowedRoles={["admin", "moderator", "visitor"]}
-                  >
-                    <AddressDetailsPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/address/edit/:addressId"
-                element={
-                  <PrivateRoute allowedRoles={["admin", "moderator"]}>
-                    <EditAddressPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/address/add/:userId"
-                element={
-                  <PrivateRoute allowedRoles={["admin", "moderator"]}>
-                    <AddAddressPage />
-                  </PrivateRoute>
-                }
-              />
+                <Route
+                  path="/address/:addressId"
+                  element={
+                    <PrivateRoute
+                      allowedRoles={["admin", "moderator", "visitor"]}
+                    >
+                      <AddressDetailsPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/address/edit/:addressId"
+                  element={
+                    <PrivateRoute allowedRoles={["admin", "moderator"]}>
+                      <EditAddressPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/address/add/:userId"
+                  element={
+                    <PrivateRoute allowedRoles={["admin", "moderator"]}>
+                      <AddAddressPage />
+                    </PrivateRoute>
+                  }
+                />
 
-              <Route
-                path="/crime/edit/:crimeId"
-                element={
-                  <PrivateRoute allowedRoles={["admin", "moderator"]}>
-                    <EditCrimePage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/crime/add/:userId"
-                element={
-                  <PrivateRoute allowedRoles={["admin", "moderator"]}>
-                    <AddCrimePage />
-                  </PrivateRoute>
-                }
-              />
+                <Route
+                  path="/crime/edit/:crimeId"
+                  element={
+                    <PrivateRoute allowedRoles={["admin", "moderator"]}>
+                      <EditCrimePage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/crime/add/:userId"
+                  element={
+                    <PrivateRoute allowedRoles={["admin", "moderator"]}>
+                      <AddCrimePage />
+                    </PrivateRoute>
+                  }
+                />
 
-              <Route
-                path="/"
-                element={
-                  <Navigate
-                    to={isLoggedIn === true ? "/users" : "/auth"}
-                    replace
-                  />
-                }
-              />
+                <Route
+                  path="/"
+                  element={
+                    <Navigate
+                      to={isLoggedIn === true ? "/users" : "/auth"}
+                      replace
+                    />
+                  }
+                />
 
-              <Route path="/no-access" element={<NoAccessPage />} />
-              <Route path="*" element={<ErrorPage />} />
-            </Routes>
-          </Suspense>
-        </Container>
-      </main>
+                <Route path="/no-access" element={<NoAccessPage />} />
+                <Route path="*" element={<ErrorPage />} />
+              </Routes>
+            </Suspense>
+          </Container>
+        </main>
+
+        <Footer />
+      </Box>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/layouts/Footer/index.tsx
@@ -0,0 +1,26 @@
+import { Box, Container, Typography } from "@mui/material";
+import { useAppSelector } from "../../redux/hooks";
+
+const Footer = () => {
+  const lightTheme = useAppSelector((state) => state.theme.lightTheme);
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        backgroundColor: lightTheme ? "#6482AD" : "#27374D",
+        color: "#fff",
+        padding: "10px 0",
+        textAlign: "center",
+        marginTop: "auto",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Typography variant="body1">
+          © {new Date().getFullYear()} My Company. All rights reserved.
+        </Typography>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
